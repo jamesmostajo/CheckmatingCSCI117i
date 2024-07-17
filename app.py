9ae0ecc0262289e47fd650607e0cc87d678a4638
Bryan <franciscobryaaan@gmail.com>
2024-07-17 21:40:40 +0800

    initialized heatmap config

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
 import streamlit as st
+import random
+# import pyfile
+
+random.seed(121415)
 
 
 @st.cache_data
@@ -9,12 +13,30 @@ def get_data():
 
     return
 
+def show_checkmate_heatmap():
+    import plotly.express as px
+    
+    st.subheader("Where do pieces move to checkmate?")
+
+    data = [[random.randint(1, 1000) for _ in range(8)] for _ in range(8)]
+    # data = pyfile.func()
+
+    fig = px.imshow(data,
+                    labels=dict(color="Checkmates"),
+                    x=list("abcdefgh"),
+                    y=list("12345678"),
+                    height = 690
+                   )
+
+
+    st.plotly_chart(fig, theme="streamlit")
+
 
 def main():
     st.set_page_config(
         page_title="Chess Visualizations", 
         page_icon=":chess_pawn:", 
-        layout="wide",
+        layout="centered",
         menu_items={
             'About': "This app is made for a Data Visualization class in Ateneo de Manila University."
         }
@@ -27,7 +49,7 @@ def main():
 
     # Filters
     st.write("Filters")
-    col1, col2, col3 = st.columns(3)
+    col1, col2, col3 = st.columns(3, gap="medium")
     with col1:
         time_control = option = st.selectbox(
             "Time Control",
@@ -36,14 +58,16 @@ def main():
     with col2:
         start_elo, end_elo = st.select_slider(
             "Select a range of elo rating",
-            options=range(2300, 3001),
-            value=(2300, 3000)
+            options=range(2300, 3532),
+            value=(2300, 3531)
         )
     with col3:
         opening = option = st.selectbox(
             "Select an opening",
             ("All", "A", "B", "C")
         )
+
+    show_checkmate_heatmap()
 
 
 if __name__ == '__main__':
