cca22169bafcabfa981ca322995f60ad49aa7835
Bryan <franciscobryaaan@gmail.com>
2024-07-18 00:14:56 +0800

    made MateElo.csv, moved MatePieces to Cleaning

--- Cleaning/ExtractCheckmatesElo.py
+++ Cleaning/ExtractCheckmatesElo.py
@@ -2,8 +2,9 @@ import csv
 
 elofile = "EloInfo.csv"
 matefile = "MatePieces.csv"
-
+outfile = "MateElo.csv"
 elo = []
+mate = {}
 
 with open(elofile, 'r') as elof:
     eloreader = csv.reader(elof)
@@ -11,5 +12,19 @@ with open(elofile, 'r') as elof:
     for row in eloreader:
         elo.append(row)
 
-for i in range(5):
-    print(elo[i])
+with open(matefile, 'r') as matef:
+    matereader = csv.reader(matef)
+
+    for row in matereader:
+        mate[row[0]] = row
+
+out = []
+for row in elo:
+    if row[0] not in mate:
+        continue
+    out.append(mate[row[0]] + [row[1]] + [row[2]])
+
+for row in out:
+    print(*row, sep=',')
+
+# python3 ExtractCheckmatesElo.py > MateElo.csv
